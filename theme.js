d1f53b9f2742e421a1e10527d33837217576190b
Funx <geraud.henrion@gmail.com>
2019-06-24 23:57:11 +0200

    sketch out notes

---
+++ theme.js
@@ -0,0 +1,11 @@
+// example theme.js
+export default {
+  // add a custom font
+  font: "Catamaran, Roboto, sans-serif",
+  // custom colors
+  colors: {
+    text: "#f0f",
+    background: "black",
+    link: "#0ff"
+  }
+};
